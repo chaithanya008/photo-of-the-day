f520171f4a6f4e6d35544fa136efb57b5ac2d933
chaithanya008 <krishna.chaithanya.8@gmail.com>
2020-04-13 23:24:58 +0400

    adding upload and choose file buttons & img file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MemeComponent } from './meme/meme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/meme/meme.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: "app-meme",
+  templateUrl: "./meme.component.html",
+  styleUrls: ["./meme.component.scss"],
+})
+export class MemeComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  selectedFile = null;
+
+  ngOnInit() {}
+
+  onFileSelected(event) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  onUpload() {
+    this.http.post('')
+  }
+}
